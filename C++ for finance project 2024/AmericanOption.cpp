3a45c7d5465bbabd8245c43c65ce9df352a61be5
Mouhamedddd <m.diallo632@hotmail.fr>
2025-03-13 16:23:47 +0100

    Files Update

--- C++ for finance project 2024/AmericanOption.cpp
+++
@@ -1,8 +0,0 @@
-#include "AmericanOption.h"
-//constructor for american option class
-AmericanOption::AmericanOption(double _expiry):Option(_expiry){}
-//returen true if its american option
-bool AmericanOption::isAmericanOption() const
-{
-	return true;
-}
